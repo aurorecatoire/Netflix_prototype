3519cf0cc3f70c39842e57c6989db88924b869ab
aurore catoire <aurore.catoire@gmail.com>
2025-11-11 16:39:12 +0100

    Il manque le carrousel

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,28 @@ import LogoNetflix from "./img/logo.png";
 
 function App() {
   return (
-    <header>
-      <h1>titde</h1>
-      <img alt="LogoNetflix" src={LogoNetflix} />
-    </header>
+    <div>
+      <header>
+        <img className="LogoNetflix" alt="Logo Netflix" src={LogoNetflix} />
+      </header>
+
+      <main>
+        {Movies.map((CategoryofMovie, index) => (
+          <div className="Element">
+            <h2>{CategoryofMovie.category}</h2>
+            <div className="allMovies">
+              {CategoryofMovie.images.map((MoviesUrl, MoviesIndex) => (
+                <img
+                  key={MoviesIndex}
+                  src={MoviesUrl}
+                  alt={`${CategoryofMovie} ${MoviesIndex}`}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+      </main>
+    </div>
   );
 }
 
