340c9609f269554405e0f73938a0c033d933a2b7
aurore catoire <aurore.catoire@gmail.com>
2025-11-11 14:48:58 +0100

    Premier commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Movies from "../movies_rnexgr.json";
+import LogoNetflix from "./img/logo.png";
+
+function App() {
+  return (
+    <header>
+      <h1>titde</h1>
+      <img alt="LogoNetflix" src={LogoNetflix} />
+    </header>
+  );
+}
+
+export default App;
